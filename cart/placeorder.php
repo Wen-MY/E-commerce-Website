cb9aacc032c306271c6942af828b452b8355f171
Yong Yao Wen <89002614+Wen-MY@users.noreply.github.com>
2023-07-01 14:35:27 +0800

    Add files via upload

---
+++ cart/placeorder.php
@@ -0,0 +1,11 @@
+
+
+<div class="placeorder content-wrapper">
+	<button onclick="myFunction()">Try it</button>
+
+	<p id="demo"></p>
+	
+    <h1>Your Order Has Been Placed</h1>
+    <p>Thank you for ordering with us! We'll contact you by email with your order details.</p>
+</div>
+
